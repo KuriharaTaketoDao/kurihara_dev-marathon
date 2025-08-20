e81244e8d907272b3651ecd0ddcd2e2f04f3a083
Kurihara Taketo <taketo.kurihara@rakus-partners.co.jp>
2025-08-20 14:38:31 +0900

    22KM

--- src/web/config.js
+++ src/web/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: 'http://dev.marathon.rplearn.net/taketo_kurihara/customer'
+  apiUrl: 'http://dev.marathon.rplearn.net/taketo_kurihara'  // /customer は削除
 };
 
 export default config;
