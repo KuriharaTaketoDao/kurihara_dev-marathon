e22ea62077413322269318f3b5eccfb0d2bc4def
Kurihara Taketo <taketo.kurihara@rakus-partners.co.jp>
2025-08-22 09:38:18 +0900

    28KM

--- src/node/index.js
+++ src/node/index.js
@@ -23,7 +23,7 @@ const pool = new Pool({
   user: "user_taketo_kurihara",
   host: "localhost",
   database: "db_taketo_kurihara",
-  password: "pass", // 実際のパスワード
+  password: "5Rw5YDaWc5jc", // 実際のパスワード
   port: 5432,
 });
 
